951b6061cd639d40dae2d2387eb5ef709a2cb4bb
Michael Federczuk <federczuk.michael@protonmail.com>
2023-03-07 18:16:32 +0100

    Check if in repo in git sync completion function

--- shell/shells/bash/completions/git/git-sync.bash
+++ shell/shells/bash/completions/git/git-sync.bash
@@ -6,12 +6,16 @@ fi
 
 # for command usage, i.e.: `git sync`
 function _git_sync() {
+	compopt +o bashdefault +o default
+
 	if ((COMP_CWORD == 2)); then
-		__gitcomp "$(git --no-pager remote)"
+		if git --no-pager rev-parse 2> '/dev/null'; then
+			__gitcomp "$(git --no-pager remote)"
+		fi
+
 		return
 	fi
 
-	compopt +o bashdefault +o default
 	__gitcomp
 }
 
@@ -19,7 +23,10 @@ function _git_sync() {
 if command -v __git_complete > '/dev/null'; then
 	function _git_sync_completion() {
 		if ((COMP_CWORD == 1)); then
-			__gitcomp "$(git --no-pager remote)"
+			if git --no-pager rev-parse 2> '/dev/null'; then
+				__gitcomp "$(git --no-pager remote)"
+			fi
+
 			return
 		fi
 
@@ -31,10 +38,13 @@ if command -v __git_complete > '/dev/null'; then
 else
 	function _git_sync_completion() {
 		if ((COMP_CWORD == 1)); then
-			local remotes
-			remotes="$(git --no-pager remote)"
+			if git --no-pager rev-parse 2> '/dev/null'; then
+				local remotes
+				remotes="$(git --no-pager remote)"
 
-			mapfile -t COMPREPLY < <(compgen -W "$remotes" -- "$2")
+				mapfile -t COMPREPLY < <(compgen -W "$remotes" -- "$2")
+			fi
+
 			return
 		fi
 
